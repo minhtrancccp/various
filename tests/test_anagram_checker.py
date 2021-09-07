6dfaae2d9b5ed7d3e070de8fdc832d8d1dd91151
Minh Tran <33189614+minhtrancccp@users.noreply.github.com>
2021-09-07 02:47:04 +0100

    Set up TODO

--- tests/test_anagram_checker.py
+++ tests/test_anagram_checker.py
@@ -1,8 +1,16 @@
+"""
+TODO:
+    - Rewrite into at least 3 functions: one for passing, one for not passing, and one for wrong data types
+    - Add time measurement for the passing tests
+"""
 from codetiming import Timer
 
-from various.anagram_checker import are_anagrams
+from various.anagram_checker import Anagram
+
+english_anagram_checker: Anagram = Anagram()
 
 
 @Timer(text="\n{} seconds")
 def test_anagram_check():
-    assert are_anagrams("Silent", "Listen")
+    assert english_anagram_checker.are_anagrams("Silent", "Listen")
+    assert not english_anagram_checker.are_anagrams("when", "what")
--- various/anagram_checker.py
+++ various/anagram_checker.py
@@ -1,32 +1,36 @@
 """
 References:
     https://twitter.com/fermatslibrary/status/1275066521450975234
+
+TODO:
+    - Explain why 27th prime is needed when defining Anagram object
+    - Consider to implement acceptance of self defined alphabet
+    - Add documentation for the are_anagrams method
 """
 from math import prod
 from string import ascii_lowercase
 
 from beartype import beartype
 from more_itertools import all_equal
-from sympy import prime, primerange
+from sympy import primerange
 
 
-class _ProductEquivalent:
+class Anagram:
     def __init__(self):
         twenty_seventh_prime: int = 103
         self._char_prime_dict: dict[str, int] = dict(
             zip(ascii_lowercase, primerange(2, twenty_seventh_prime))
         )
 
-    def word_to_product(self, word: str) -> int:
+    def _word_to_product(self, word: str) -> int:
         return prod(
             self._char_prime_dict[char] for char in word.lower() if char.isalpha()
         )
 
+    @beartype
+    def are_anagrams(self, *words: str) -> bool:
+        """
+        Determine whether given strings are anagrams by calculating the product of primes
+        """
 
-@beartype
-def are_anagrams(*words: str) -> bool:
-    """
-    Determine whether given strings are anagrams by calculating the product of primes
-    """
-    machine: _ProductEquivalent = _ProductEquivalent()
-    return all_equal(map(machine.word_to_product, words))
+        return all_equal(map(self._word_to_product, words))
