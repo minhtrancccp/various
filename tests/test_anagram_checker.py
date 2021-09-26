c5aec17a9eb4a5bc6b7fb3c1e46cf814dbe17875
Minh Tran <33189614+minhtrancccp@users.noreply.github.com>
2021-09-26 03:32:53 +0100

    feat: Add beartype check for parameters and refactor the old class into independent functions
    Signed-off-by: Minh Tran <33189614+minhtrancccp@users.noreply.github.com>

--- tests/test_anagram_checker.py
+++ tests/test_anagram_checker.py
@@ -1,31 +1,56 @@
-from pytest import fixture, mark
+"""
+TODO:
+    - Use hypothesis to conduct property-based tests
+"""
 
-from miscellaneous_python.anagram_checker import Anagram
+from re import escape
 
+from beartype.roar import BeartypeCallHintPepParamException
+from pytest import mark, raises
 
-@fixture
-def anagram_machine() -> Anagram:
-    return Anagram()
+from miscellaneous_python.anagram import anagram_checker
+
+ParamsType: type[tuple] = tuple[str, ...]
 
 
 @mark.parametrize(
     "are_anagrams_params",
     [
-        ("where",),
         ("Silent", "Listen"),
         ("Anagram", "Nag a ram"),
         ("liter", "litre", "tiler"),
         ("ArE", "eAr"),
-        # ("$&%367#%&56", " 5&467865{|:$ 5"),
     ],
 )
-def test_are_anagrams(anagram_machine: Anagram, are_anagrams_params: tuple[str, ...]):
-    assert anagram_machine.are_anagrams(*are_anagrams_params)
+def test_are_anagrams(are_anagrams_params: ParamsType):
+    assert anagram_checker(are_anagrams_params)
 
 
 @mark.parametrize(
     "not_anagrams_params",
     [("where", "when"), ("nag", "Gram"), ("liter", "litre", "tiler", "peril")],
 )
-def test_not_anagrams(anagram_machine: Anagram, not_anagrams_params: tuple[str, ...]):
-    assert not anagram_machine.are_anagrams(*not_anagrams_params)
+def test_not_anagrams(not_anagrams_params: ParamsType):
+    assert not anagram_checker(not_anagrams_params)
+
+
+@mark.parametrize("not_enough_params", [tuple(), ("where",)])
+def test_not_enough_params(not_enough_params: ParamsType):
+    not_enough_message: str = escape("violates validator Is[_has_sufficient_length()]")
+    with raises(
+        BeartypeCallHintPepParamException,
+        match=not_enough_message,
+    ):
+        anagram_checker(not_enough_params)
+
+
+@mark.parametrize(
+    "not_latin_params", [("$&%367#%&56", " 5&467865{|:$ 5"), ("情人", "人情")]
+)
+def test_not_latin_params(not_latin_params: ParamsType):
+    not_latin_message: str = escape("violates validator Is[has_latin_letters()]")
+    with raises(
+        BeartypeCallHintPepParamException,
+        match=not_latin_message,
+    ):
+        anagram_checker(not_latin_params)
