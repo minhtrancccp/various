c654c5f8bb35878989394fa35c8bd5ff57359886
Minh Tran <33189614+minhtrancccp@users.noreply.github.com>
2021-10-16 23:08:39 +0200

    perf: ⚡ improve basic_latin_letters script
    simplify STRING_PATTERN
    
    BREAKING CHANGE: replace latin_filter function's return type to a list of matched Latin letters
    
    Signed-off-by: Minh Tran <33189614+minhtrancccp@users.noreply.github.com>

--- miscellaneous_python/type_hints/basic_latin_letters.py
+++ miscellaneous_python/type_hints/basic_latin_letters.py
@@ -7,7 +7,6 @@ Notes:
     - A valid Latin string must contain at least one (1) Latin letter as defined in the Basic Latin Unicode block
 """
 import re
-from collections.abc import Iterator
 from typing import Annotated
 
 import regex
@@ -15,24 +14,20 @@ from beartype import beartype
 from beartype.vale import Is
 
 LETTER_PATTERN: re.Pattern[str] = re.compile(r"[a-zA-Z]")
-STRING_PATTERN: regex.Pattern[str] = regex.compile(
-    r"[\p{BasicLatin}||\P{Alphabetic}]+$"
-)
+STRING_PATTERN: regex.Pattern[str] = regex.compile(r"[\p{BasicLatin}||\P{Alphabetic}]+")
 
 
 @beartype
 def string_validator(string: str) -> bool:
-    return bool(LETTER_PATTERN.search(string) and STRING_PATTERN.match(string))
+    return bool(LETTER_PATTERN.search(string) and STRING_PATTERN.fullmatch(string))
 
 
 ValidatedStringType: type[str] = Annotated[str, Is[string_validator]]
 
 
 @beartype
-def latin_filter(string: ValidatedStringType, lowercase: bool = True) -> Iterator[str]:
+def latin_filter(string: ValidatedStringType, lowercase: bool = True) -> list[str]:
     if lowercase:
         string = string.lower()
 
-    found_letter: re.Match[str]
-    for found_letter in LETTER_PATTERN.finditer(string):
-        yield re.Match.group(found_letter)
+    return LETTER_PATTERN.findall(string)
