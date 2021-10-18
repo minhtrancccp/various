3e98c1cdfca5d5d076798c161aac888759fa5dca
Minh Tran <33189614+minhtrancccp@users.noreply.github.com>
2021-10-19 00:18:33 +0200

    feat: ✨ rewrite climbing_snail.py
    A new class Snail is created with the following methods:
    - normal_snail() checks if snail has bigger forwards speed than backwards speed
    - timer() calculates min climbing time with different distances
    
    Signed-off-by: Minh Tran <33189614+minhtrancccp@users.noreply.github.com>

--- miscellaneous_python/climbing_snail.py
+++ miscellaneous_python/climbing_snail.py
@@ -3,18 +3,28 @@ References:
     https://codegolf.stackexchange.com/questions/130290/the-snail-in-the-well
 """
 from math import ceil
-from numbers import Real
 from typing import Optional
 
 from beartype import beartype
 
+from auxiliary.real_numbers import PositiveReal
 
-@beartype
-def time_finder(distance: Real, forward: Real, backward: Real) -> Optional[int]:
-    if distance <= forward:
-        return 1
 
-    if forward <= backward:
-        return None
+class Snail:
+    @beartype
+    def __init__(self, forward: PositiveReal, backward: PositiveReal) -> None:
+        self.forward: PositiveReal = forward
+        self.backward: PositiveReal = backward
 
-    return ceil((distance - forward) / (forward - backward)) + 1
+    def normal_snail(self) -> bool:
+        return self.forward > self.backward
+
+    @beartype
+    def timer(self, distance: PositiveReal) -> Optional[PositiveReal]:
+        if distance <= self.forward:
+            return 1
+
+        if not self.normal_snail():
+            return None
+
+        return ceil((distance - self.forward) / (self.forward - self.backward)) + 1
--- tests/test_climbing_snail.py
+++ tests/test_climbing_snail.py
@@ -1,21 +1,39 @@
-from pytest import mark
+from numbers import Real
+from typing import Optional
 
-from miscellaneous_python.climbing_snail import time_finder
+from hypothesis import example, given
+from hypothesis.strategies import SearchStrategy, floats
 
+from auxiliary.real_numbers import is_positive_real
+from miscellaneous_python.climbing_snail import Snail
 
-@mark.parametrize(
-    "inputs, output",
-    [
-        ((30, 3, 2), 28),
-        ((84, 17, 15), 35),
-        ((79, 15, 9), 12),
-        ((29, 17, 4), 2),
-        ((13, 18, 8), 1),
-        ((5, 5, 10), 1),
-        ((7, 7, 7), 1),
-        ((69, 3, 8), None),
-        ((81, 14, 14), None),
-    ],
+positive_real_strategies: SearchStrategy[float] = floats(
+    min_value=0,
+    exclude_min=True,
+    allow_nan=False,
+    allow_infinity=False,
 )
-def test_time_finder(inputs, output):
-    assert time_finder(*inputs) == output
+
+
+@example(30, 3, 2)
+@example(84, 17, 15)
+@example(79, 15, 9)
+@example(29, 17, 4)
+@example(13, 18, 8)
+@example(5, 5, 10)
+@example(7, 7, 7)
+@example(69, 3, 8)
+@example(81, 14, 14)
+@given(positive_real_strategies, positive_real_strategies, positive_real_strategies)
+def test_timer(forward: Real, backward: Real, distance: Real) -> None:
+    snail: Snail = Snail(forward, backward)
+
+    result: Optional[Real] = snail.timer(distance)
+    if distance <= snail.forward:
+        assert result == 1
+
+    elif not snail.normal_snail():
+        assert result is None
+
+    else:
+        assert is_positive_real(result)
