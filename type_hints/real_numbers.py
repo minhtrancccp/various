b9d688cf0caeeb493fe1b36f2361aa9c14dbfde8
Minh Tran <33189614+minhtrancccp@users.noreply.github.com>
2021-11-10 00:08:58 +0100

    fix: 🐛 replace numerary type hints
    A replacement from IntegralLike & RealLike to IntegralLikeSCU & RealLikeSCU is made as the former version raises type errors in production.
    
    Signed-off-by: Minh Tran <33189614+minhtrancccp@users.noreply.github.com>

--- type_hints/real_numbers.py
+++ type_hints/real_numbers.py
@@ -2,21 +2,31 @@ from typing import Annotated
 
 from beartype import beartype
 from beartype.vale import Is
-from numerary.types import IntegralLike, RealLike
+from numerary.types import IntegralLikeSCU, RealLikeSCU
 
 
 @beartype
-def is_not_negative(value: RealLike) -> bool:
+def is_not_negative(value: RealLikeSCU) -> bool:
+    """
+    Return a boolean indicating whether the given real number is not negative.
+    """
+
     return value >= 0
 
 
 @beartype
-def is_positive_real(value: RealLike) -> bool:
+def is_positive_real(value: RealLikeSCU) -> bool:
+    """
+    Return a boolean indicating whether the given real number is positive.
+    """
+
     return value > 0
 
 
-NaturalNumber: type[IntegralLike] = Annotated[IntegralLike, Is[is_not_negative]]
-PositiveInteger: type[IntegralLike] = Annotated[IntegralLike, Is[is_positive_real]]
+NaturalNumber: type[IntegralLikeSCU] = Annotated[IntegralLikeSCU, Is[is_not_negative]]
+PositiveInteger: type[IntegralLikeSCU] = Annotated[
+    IntegralLikeSCU, Is[is_positive_real]
+]
 
-NonNegativeReal: type[RealLike] = Annotated[RealLike, Is[is_not_negative]]
-PositiveReal: type[RealLike] = Annotated[RealLike, Is[is_positive_real]]
+NonNegativeReal: type[RealLikeSCU] = Annotated[RealLikeSCU, Is[is_not_negative]]
+PositiveReal: type[RealLikeSCU] = Annotated[RealLikeSCU, Is[is_positive_real]]
