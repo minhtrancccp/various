3fb498f29f2723379357e1873520e21418f23d2d
Minh Tran <33189614+minhtrancccp@users.noreply.github.com>
2021-11-10 00:13:25 +0100

    refactor: ♻ update climbing_snail.py
    Add docstrings to the class of Snail's methods, as well as fixing the method timer's return type hint to natural value
    
    Signed-off-by: Minh Tran <33189614+minhtrancccp@users.noreply.github.com>

--- miscellaneous_python/climbing_snail.py
+++ miscellaneous_python/climbing_snail.py
@@ -6,20 +6,35 @@ from math import ceil
 
 from beartype import beartype
 
-from type_hints.real_numbers import NonNegativeReal, PositiveReal
+from type_hints.real_numbers import NaturalNumber, PositiveReal
 
 
 class Snail:
+    """
+    A class for emulating a wall-climbing snail
+    """
+
     def __init__(self, forward: PositiveReal, backward: PositiveReal) -> None:
         self.forward: PositiveReal = forward
         self.backward: PositiveReal = backward
 
     @beartype
     def normal_snail(self) -> bool:
+        """
+        Return a boolean indicating if the snail is normal, i.e. its forward speed is greater than its backward speed
+        """
+
         return self.forward > self.backward
 
     @beartype
-    def timer(self, distance: PositiveReal) -> NonNegativeReal:
+    def timer(self, distance: PositiveReal) -> NaturalNumber:
+        """
+        Return the time the snail takes to travel a distance
+
+        If the travel distance isn't greater than the snail's forward speed and the snail is not normal, 0 is returned.
+        To check if the snail is normal, use the Snail.normal_snail method.
+        """
+
         if distance <= self.forward:
             return 1
 
