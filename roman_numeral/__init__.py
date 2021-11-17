31bc918fda6b43563ed506d5fabe03da82d7be3c
Minh Tran <33189614+minhtrancccp@users.noreply.github.com>
2021-11-17 14:32:54 +0100

    feat: ✨ new subpackage roman_numeral
    BREAKING CHANGE: 💥 roman_numeral script in miscellaneous_python subpackage replaced by a subpackage of the same name
    
    The Roman decoding and encoding functionalities in the old script have grown so sophisticated that it can not be maintained as well as it should be. That's why it has been converted into a subpackage on its own.
    
    Signed-off-by: Minh Tran <33189614+minhtrancccp@users.noreply.github.com>

---
+++ roman_numeral/__init__.py
@@ -0,0 +1,9 @@
+"""
+References:
+    https://en.wikipedia.org/wiki/Roman_numerals#Standard_form
+"""
+
+from roman_numeral.decoder_module import decoder
+from roman_numeral.encoder_module import encoder
+
+__all__ = ["decoder", "encoder"]
