5458532fcc56066f06b837addbcadeb0b47211cd
Minh Tran <33189614+minhtrancccp@users.noreply.github.com>
2021-12-19 01:02:11 +0100

    test: ✅ lower the limit in test_recursive_fibonacci.py

--- tests/test_miscellaneous_python/test_recursive_fibonacci.py
+++ tests/test_miscellaneous_python/test_recursive_fibonacci.py
@@ -7,7 +7,7 @@ from data_filter.real_numbers import PositiveInteger
 from miscellaneous_python.recursive_fibonacci import recursive_function
 
 # To avoid 'RecursionError: maximum recursion depth exceeded', a max absolute limit is set for testing
-max_abs_limit: PositiveInteger = 330
+max_abs_limit: PositiveInteger = 325
 
 
 @given(integers(min_value=-max_abs_limit, max_value=max_abs_limit))
